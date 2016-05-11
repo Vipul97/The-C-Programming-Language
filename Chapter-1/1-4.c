ccb76182f2a5c3b1cd64a9e1b7c47561490d7e8b
Vipul97 <vipul.gharde@gmail.com>
2016-05-11 23:17:21 +0300

    Add files via upload

---
+++ Chapter-1/1-4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* print Celsius-Fahrenheit table
+for celsius = 0, 20, ..., 300; floating-point version */
+main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	celsius = lower;
+	while (celsius <= upper) {
+		fahr = (9.0 / 5.0) * celsius + 32.0;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+	getch();
+}
